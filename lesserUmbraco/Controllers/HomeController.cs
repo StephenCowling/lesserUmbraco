28047cf7846e69991ea20e2aefc05601dd5e8afb
Steve Cowling <stephen.cowling@kiwiplates.nz>
2017-10-10 12:07:05 +1300

    Add SiteLayout and Home Controllers

---
+++ lesserUmbraco/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+namespace lesserUmbraco.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
+
+        public ActionResult RenderFeatured()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Featured.cshtml");
+        }
+    }
+}
