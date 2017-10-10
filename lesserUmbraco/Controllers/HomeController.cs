770414522f0d601683a87ddaf2e0bed4ac641108
Steve Cowling <stephen.cowling@kiwiplates.nz>
2017-10-11 12:35:20 +1300

    Build Dynamic navigation

--- lesserUmbraco/Controllers/HomeController.cs
+++ lesserUmbraco/Controllers/HomeController.cs
@@ -12,9 +12,25 @@ namespace lesserUmbraco.Controllers
 
         private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
 
+        public ActionResult RenderIntro()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
+        }
         public ActionResult RenderFeatured()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Featured.cshtml");
+        }
+        public ActionResult RenderServices()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Services.cshtml");
+        }
+        public ActionResult RenderBlog()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Blog.cshtml");
+        }
+        public ActionResult RenderClients()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Clients.cshtml");
         }
     }
 }
--- lesserUmbraco/Controllers/SiteLayoutController.cs
+++ lesserUmbraco/Controllers/SiteLayoutController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using Umbraco.Web.Mvc;
 using System.Web.Mvc;
+using lesserUmbraco.Models;
+using Umbraco.Core.Models;
 
 namespace lesserUmbraco.Controllers
 {
@@ -11,17 +13,55 @@ namespace lesserUmbraco.Controllers
     {
         private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
 
-        public ActionResult RenderHeader()
-        {
-            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
-        }
-        public ActionResult RenderIntro()
-        {
-            return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
-        }
         public ActionResult RenderFooter()
         {
             return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
+        /// <summary>
+        /// Renders the top navigation in the header partial
+        /// </summary>
+        /// <returns>Partial view with a model</returns>
+        public ActionResult RenderHeader()
+        {
+            List<NavigationListItem> nav = GetNavigationModelFromDatabase();
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml", nav);
+        }
+
+        /// <summary>
+        /// Finds the home page and gets the navigation structure based on it and it's children
+        /// </summary>
+        /// <returns>A List of NavigationListItems, representing the structure of the site.</returns>
+        private List<NavigationListItem> GetNavigationModelFromDatabase()
+        {
+            const int HOME_PAGE_POSITION_IN_PATH = 1;
+            int homePageId = int.Parse(CurrentPage.Path.Split(',')[HOME_PAGE_POSITION_IN_PATH]);
+            IPublishedContent homePage = Umbraco.Content(homePageId);
+            List<NavigationListItem> nav = new List<NavigationListItem>();
+            nav.Add(new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name)));
+            nav.AddRange(GetChildNavigationList(homePage));
+            return nav;
+        }
+
+        /// <summary>
+        /// Loops through the child pages of a given page and their children to get the structure of the site.
+        /// </summary>
+        /// <param name="page">The parent page which you want the child structure for</param>
+        /// <returns>A List of NavigationListItems, representing the structure of the pages below a page.</returns>
+        private List<NavigationListItem> GetChildNavigationList(dynamic page)
+        {
+            List<NavigationListItem> listItems = null;
+            var childPages = page.Children.Where("Visible");
+            if (childPages != null && childPages.Any() && childPages.Count() > 0)
+            {
+                listItems = new List<NavigationListItem>();
+                foreach (var childPage in childPages)
+                {
+                    NavigationListItem listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name));
+                    listItem.Items = GetChildNavigationList(childPage);
+                    listItems.Add(listItem);
+                }
+            }
+            return listItems;
         }
     }
 }
